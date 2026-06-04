1a4b86f2c20b8a4a1d217f0d64f74c8827c393c0
CouponXpertbot <yuvrajkr2203@yahoo.com>
2026-06-04 16:16:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,14 @@
 import requests
 
-html = requests.get("https://coursefolder.net/", timeout=20).text
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/125.0 Safari/537.36"
+}
 
-print(html[:5000])
+response = requests.get(
+    "https://coursefolder.net/",
+    headers=headers,
+    timeout=20
+)
+
+print(response.status_code)
+print(response.text[:1000])
