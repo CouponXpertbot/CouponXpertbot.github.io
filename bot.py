0d826a77f89029a02ad259eda1295ed544cbdd98
CouponXpertbot <yuvrajkr2203@yahoo.com>
2026-06-04 16:30:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
 import requests
+from bs4 import BeautifulSoup
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/125.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0"
 }
 
-response = requests.get(
+html = requests.get(
     "https://coursefolder.net/",
     headers=headers,
     timeout=20
-)
+).text
 
-print(response.status_code)
-print(response.text[:1000])
+soup = BeautifulSoup(html, "html.parser")
+
+# Print all links containing "coursefolder.net/"
+for a in soup.find_all("a", href=True):
+    href = a["href"]
+
+    if "coursefolder.net/" in href:
+        print("TEXT:", a.get_text(strip=True))
+        print("LINK:", href)
+        print("-" * 50)
