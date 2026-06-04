bcf3a4828601d203651b966d22067edf22822a5f
CouponXpertbot <yuvrajkr2203@yahoo.com>
2026-06-04 20:04:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,16 @@
 import requests
-from bs4 import BeautifulSoup
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
+url = "https://elearn.interviewgig.com/free-online-courses-coupons/"
 
 html = requests.get(
-    "https://coursefolder.net/",
-    headers=headers,
-    timeout=20
-).text
+    url,
+    headers={"User-Agent": "Mozilla/5.0"},
+    timeout=30
+)
 
-soup = BeautifulSoup(html, "html.parser")
+print(html.status_code)
 
-print("TITLE:", soup.title)
+with open("page.html", "w", encoding="utf-8") as f:
+    f.write(html.text)
 
-print("\nFIRST 20 LINKS:\n")
-
-for i, a in enumerate(soup.find_all("a", href=True)[:20]):
-    print(i + 1)
-    print("TEXT:", a.get_text(strip=True))
-    print("HREF:", a["href"])
-    print("-" * 40)
+print("saved")
